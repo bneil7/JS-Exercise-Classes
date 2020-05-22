a044c170decb4a522f471dccb0cca92c21a25052
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-05-22 09:01:41 -0600

    last commit push before i start sprint. still didnt quite finish this one unfortunately..

--- index.js
+++ index.js
@@ -75,7 +75,26 @@ console.log(mary.toString());
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank += gallons
+  }
+  drive(distance){
+    // if statement conditional that determines if the distance/mpg is greater than tank
+    // will return out of fuel at odometer
+    if (distance/this.milesPerGallon > this.tank) {
+      this.drive(this.milesPerGallon * this.tank);
+      return `I ran out of fuel at ${this.odometer}`
+    }
+    // otherwise we will just add distance to odometer and tank will subtract distance/mpg
+    this.odometer += distance;
+    this.tank -= distance/this.milesPerGallon;
+  }
 }
 
 /*
@@ -91,8 +110,16 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(attributes){
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}.`
+  }
 }
+
 
 /*
   TASK 4
